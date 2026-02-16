8d04b10c11a15ea8969b18384adb068f8c5cd30e
webDevT <karinadesyatnik27@gmail.com>
2026-02-16 08:13:04 +0200

    build issues fix

--- src/pages/about/components/index.tsx
+++ src/pages/about/components/index.tsx
@@ -3,5 +3,5 @@ export { default as OurCompany } from './ourCompany';
 export { default as Philosophy } from './philosophy';
 export { default as HowWeWork } from './howWeWork';
 export { default as ByTheNumbers } from './byTheNumbers';
-export { default as OurLeader } from './OurLeader';
-export { default as Advisors } from './Advisors';
+export { default as OurLeader } from './ourLeader';
+export { default as Advisors } from './advisors';
